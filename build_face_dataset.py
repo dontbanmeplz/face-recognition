4c7620e0b2a1375661a6d7fab7101be4a5447e41
Chis <59851616+dontbanmeplz@users.noreply.github.com>
2022-05-16 14:22:55 -0500

    Update build_face_dataset.py

--- build_face_dataset.py
+++ build_face_dataset.py
@@ -10,6 +10,8 @@ ap.add_argument("-c", "--cascade", required=True,
 	help = "path to where the face cascade resides")
 ap.add_argument("-o", "--output", required=True,
 	help="path to output directory")
+ap.add_argument("-i", "--input", required=True,
+	help="path to input file")
 args = vars(ap.parse_args())
 
 
@@ -19,42 +21,33 @@ detector = cv2.CascadeClassifier(args["cascade"])
 # and initialize the total number of example faces written to disk
 # thus far
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = VideoCapture(args["input"])
 # vs = VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
-total = 0
-
+t = 0
+success = True
 
 
 
 
 # loop over the frames from the video stream
-while True:
+while success:
+	if t != 6:
+		t += 1
+		success, frame = vs.read()
+		continue
+	else:
+		pass
 	# grab the frame from the threaded video stream, clone it, (just
 	# in case we want to write it to disk), and then resize the frame
 	# so we can apply face detection faster
-	frame = vs.read()
+	success, frame = vs.read()
 	orig = frame.copy()
 	frame = imutils.resize(frame, width=400)
-	# detect faces in the grayscale frame
-	rects = detector.detectMultiScale(
-		cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, 
-		minNeighbors=5, minSize=(30, 30))
-	# loop over the face detections and draw them on the frame
-	for (x, y, w, h) in rects:
-		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	# detect faces in the grayscale fram
     
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
- 
+	# show the output 
 	# if the `k` key was pressed, write the *original* frame to disk
 	# so we can later process it and use it for face recognition
-	if key == ord("k"):
-		p = os.path.sep.join([args["output"], "{}.png".format(
-			str(total).zfill(5))])
-		cv2.imwrite(p, orig)
-		total += 1
-	# if the `q` key was pressed, break from the loop
-	elif key == ord("q"):
-		break
+	
+	p = os.path.sep.join([args["output"], "{}.png".format(str(total).zfill(5))])
+	cv2.imwrite(p, orig)
